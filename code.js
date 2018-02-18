09b1a9650039dbbbd791d1ed714ee808f34a637c
jz4o <26317360+jz4o@users.noreply.github.com>
2018-02-18 23:15:47 +0900

    ドキュメンテーション及びコメントを記載

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
+// スクリプトに設定されたプロパティ一覧
 var scriptProperties = PropertiesService.getScriptProperties();
 
+// Qiita関連の各種設定値
 var qiita = {
   'apiUrl'        : 'https://qiita.com/api/v2',
   'apiToken'      : scriptProperties.getProperty('QIITA_API_ACCESS_TOKEN'),
@@ -7,33 +9,52 @@ var qiita = {
   'popularBorder' : 10
 };
 
+// Slack関連の各種設定値
 var slack = {
   'outgoingToken' : scriptProperties.getProperty('SLACK_OUTGOING_TOKEN'),
   'incomingUrl'   : scriptProperties.getProperty('SLACK_INCOMING_URL')
 };
 
+/**
+ * Qiitaの記事から以下の記事を1件ずつ取得し、Slackへ投稿します.
+ *
+ *  - ストックした記事
+ *  - フォローしたタグがつけられた記事
+ *
+ * プロジェクトトリガーを設定し、毎日実行します
+ */
 function main(){
+  // 平日以外の場合、処理を中止
   if(isHoliday()){
     return;
   }
 
+  // Qiitaでストックされた記事からランダムにSlackへ投稿
   var stockItem = getRandomQiitaItemInStock();
   if(stockItem){
     postToSlack(stockItem);
   }
 
+  // Qiitaでフォロー中のタグがつけられた記事からランダムにSlackへ投稿
   var tagItem = getRandomQiitaItemInTag();
   if(tagItem){
     postToSlack(tagItem);
   }
 }
 
+/**
+ * Slackに投稿されたQiita記事をストック及び「いいね」します.
+ *
+ * OutgoingWebhooksを経由してリクエストされる想定
+ */
 function doPost(e){
   //Slack以外からのリクエストははじく
   if(e['parameter']['token'] != slack['outgoingToken']){
     return;
   }
 
+  // 投稿内容からQiita記事のIDを取得
+  // ※取得に失敗した場合は処理を中止
   var requestText = e['parameter']['text'];
   var regexped = /\<([^\>]+)/.exec(requestText);
   if(regexped == null){
@@ -45,120 +66,229 @@ function doPost(e){
     return;
   }
 
+  // ストック及びいいねの登録
   addQiitaItemToStock(qiitaItemId);
   addQiitaItemToLike(qiitaItemId);
 }
 
+/**
+ * QiitaのURLから記事のIDを取得し、返します.
+ * ※取得に失敗した場合はNullを返します
+ *
+ * @param string url Qiita記事URL
+ *
+ * @return string / null Qiita記事ID
+ */
 function getQiitaItemId(url){
   var regexped = /https:\/\/qiita.com\/.+\/([^#?]*)/.exec(url);
   return regexped && regexped[1];
 }
 
+/**
+ * Qiitaにてフォロー中のタグがつけられた記事からランダムに1件を取得し、記事URLを返します.
+ * ※ある程度良い内容の記事を取得するため、「いいね」数によって絞込みを行います
+ * ※取得に失敗した場合はNullを返します
+ *
+ * @return string / null 記事URL
+ */
 function getRandomQiitaItemInTag(){
+  // フォロー中のタグからランダムに1件取得
+  // 取得に失敗した場合は処理を中止
   var tags = execQiitaApiForGet('users', qiita['userId'], 'following_tags');
   var randomTag = getRandomElement(tags);
   if(randomTag == null){
     return;
   }
 
+  // 上記で取得したタグのついた記事を取得
+  // 取得に失敗した場合は処理を中止
   var items = execQiitaApiForGet('tags', randomTag['id'], 'items');
   if(items.length <= 0){
     return;
   }
 
+  // 一定数以上の「いいね」がついた記事からランダムに1件取得
   var isPopularItem = function(item, index, array){
     return item['likes_count'] >= qiita['popularBorder'];
   };
   items = items.filter(isPopularItem);
   var randomItem = getRandomElement(items);
 
+  // 該当の記事URLを返す
   return randomItem && randomItem['url'];
 }
 
+/**
+ * Qiitaにてストックした記事からランダムに1件を取得し、記事URLを返します.
+ * ※取得に失敗した場合はNullを返します
+ *
+ * @return string / null 記事URL
+ */
 function getRandomQiitaItemInStock() {
+  // ストックした記事からランダムに1件取得
   var items = execQiitaApiForGet('users', qiita['userId'], 'stocks');
   var randomItem = getRandomElement(items);
   
+  //　該当の記事URLを返す
   return randomItem && randomItem['url'];
 }
 
+/**
+ * 該当のQiita記事をストック済みか確認し、結果を返します.
+ *
+ * @param string itemId 記事ID
+ *
+ * @return boolean ストック済みの場合 `true`
+ */
 function isStocked(itemId){
   return execQiitaApiForCheck('items', itemId, 'stock');
 }
 
+/**
+ * 該当のQiita記事をストックします.
+ *
+ * @param string itemId 記事ID
+ */
 function addQiitaItemToStock(itemId){
+  // すでにストック済みの場合は処理を中止
   if(isStocked(itemId)){
     return;
   }
 
+  // ストック登録
   execQiitaApiForPut('items', itemId, 'stock');
 }
 
+/**
+ * 該当のQiita記事をいいね済みか確認し、結果を返します.
+ *
+ * @param string itemId 記事ID
+ *
+ * @return boolean いいね済みの場合 `true`
+ */
 function isLiked(itemId){
   return execQiitaApiForCheck('items', itemId, 'like');
 }
 
+/**
+ * 該当のQiita記事をいいねします.
+ *
+ * @param string itemId 記事ID
+ */
 function addQiitaItemToLike(itemId){
+  // すでにいいね済みの場合は処理を中止
   if(isLiked(itemId)){
     return;
   }
 
+  // いいね登録
   execQiitaApiForPut('items', itemId, 'like');
 }
 
+/**
+ * QiitaAPIを使用し、データを取得し、返します.
+ *
+ * @param string targetGroup データ所有元の所属するグループ
+ * @param string targetId    データ所有元のID
+ * @param string targetType  取得対象のデータ種別
+ *
+ * @return array 取得結果(連想配列)の配列
+ */
 function execQiitaApiForGet(targetGroup, targetId, targetType){
+  // リクエスト対象のURL
   var url = [qiita['apiUrl'], targetGroup, targetId, targetType].join('/');
+
+  // リクエストを発行し、結果をJSONで取得
   var response = UrlFetchApp.fetch(url);
+
+  // レスポンス結果をパースし、呼び出し元に返す
   return JSON.parse(response.getContentText());
 }
 
+/**
+ * QiitaAPIを使用し、該当のデータの登録状況を取得し、返します.
+ *
+ * @param string targetGroup データ登録元の所属するグループ
+ * @param string targetId    データ登録元のID
+ * @param string targetType  確認対象のデータ種別
+ *
+ * @return boolean 登録済みの場合 `true`
+ */
 function execQiitaApiForCheck(targetGroup, targetId, targetType){
+  // リクエスト対象のURL
   var url = [qiita['apiUrl'], targetGroup, targetId, targetType].join('/');
 
+  // リクエスト設定
   var options = {
     'method' : 'get',
     'headers': {'Authorization': 'Bearer ' + qiita['apiToken']}
   };
 
   try{
+    // リクエスト発行
     UrlFetchApp.fetch(url, options);
     return true;
   }catch(e){
+    // 登録済みでない場合は例外発生
     return false;
   }
 }
 
+/**
+ * QiitaAPIを使用し、該当のデータを登録します.
+ *
+ * @param string targetGroup データ登録元の所属するグループ
+ * @param string targetId    データ登録元のID
+ * @param string targetType  登録対象のデータ種別
+ */
 function execQiitaApiForPut(targetGroup, targetId, targetType){
+  // リクエスト対象のURL
   var url = [qiita['apiUrl'], targetGroup, targetId, targetType].join('/');
 
+  // リクエスト設定
   var options = {
     'method' : 'put',
     'headers': {'Authorization': 'Bearer ' + qiita['apiToken']}
   };
 
+  // リクエスト発行
   UrlFetchApp.fetch(url, options);
 }
 
+/**
+ * Slackへメッセージを投稿します.
+ *
+ * SlackのIncomingWebhooksを経由してリクエストする想定
+ *
+ * @param string message 投稿内容
+ */
 function postToSlack(message){
+  // リクエスト設定
   var options = {
     'method'     : 'post',
     'contentType': 'application/json',
     'payload'    : JSON.stringify({'text':message, 'unfurl_links': true})
   };
 
+  // リクエスト発行
   UrlFetchApp.fetch(slack['incomingUrl'], options);
 }
 
+/**
+ * 当日の休日祝日判定を行い、結果を返します.
+ *
+ * @return boolean 当日が休日もしくは祝日の場合 `true`
+ */
 function isHoliday(){
   var today = new Date();
 
-  //土日か判定
+  //土日判定
   var weekInt = today.getDay();
   if(weekInt <= 0 || 6 <= weekInt){
     return true;
   }
 
-  //祝日か判定
+  //祝日判定
   var calendarId = "ja.japanese#holiday@group.v.calendar.google.com";
   var calendar = CalendarApp.getCalendarById(calendarId);
   var todayEvents = calendar.getEventsForDay(today);
@@ -169,6 +299,14 @@ function isHoliday(){
   return false;
 }
 
+/**
+ * 配列からランダムに要素を取得し、返します.
+ * ※配列が空の場合Nullを返します
+ *
+ * @param array 配列
+ *
+ * @return object / null 要素
+ */
 function getRandomElement(array){
   if(array.length <= 0){
     return;
